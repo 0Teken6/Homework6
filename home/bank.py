5d622994cbcddf638176de6e12f0445a30caa818
0Teken6 <timu.rev006@gmail.com>
2024-06-18 18:56:11 +0600

    second

--- home/bank.py
+++ home/bank.py
@@ -8,3 +8,44 @@ class Bank:
     def set_name(self,name):...
 
     def get_name(self):...
+
+class Bank2(Bank):
+    def __init__(self, name, age, money, key):
+        super().__init__(name, age, money, key)
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
+
+
+class Bank3(Bank2):
+    def __init__(self, name, age, money, key):
+        super().__init__(name, age, money, key)
+
+    @property
+    def age_prop(self):
+        return self._age
+
+    @age_prop.setter
+    def age_prop(self, age):
+        self._age = age
+
+    @property
+    def money_prop(self):
+        return self.__money
+
+    @money_prop.setter
+    def money_prop(self, money):
+        self.__money = money
+
+    @property
+    def key_prop(self):
+        return self.__key
+
+    @key_prop.setter
+    def key_prop(self, key):
+        self.__key = key
+
+
---
+++ home/users.py
@@ -0,0 +1,12 @@
+import bank
+
+b1 = bank.Bank3('Temirlan', 50, 1000, 196)
+
+b1.name = 'Beka'
+b1.age = 20
+b1.money = 15000
+b1.key = 221
+print(b1.name)
+print(b1.age)
+print(b1.money)
+print(b1.key)
